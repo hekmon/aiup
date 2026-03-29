89ccf1ead1ad319dc6f6a2f4a09068ff314ba542
Hekmon <edouardhur@gmail.com>
2026-03-29 11:28:54 +0200

    Refactor list formatting with generic helper

--- assistant/models/utils.go
+++ assistant/models/utils.go
@@ -7,24 +7,35 @@ const (
 )
 
 func formatListDotted(items []string, indent int) string {
-	// Prepare
-	var (
-		size    int
-		builder strings.Builder
-	)
-	for _, item := range items {
-		size += indent + len(listDottedPrefix) + len(item) + 1
-	}
-	builder.Grow(size)
-	// Build output
-	for index, item := range items {
-		builder.WriteString(strings.Repeat(" ", indent))
-		builder.WriteString(listDottedPrefix)
-		builder.WriteString(item)
-		if index != len(items)-1 {
-			builder.WriteRune('\n')
+	return formatList(items, listDottedPrefix, indent)
+}
+
+func formatList(items []string, itemPrefix string, indent int) string {
+	switch len(items) {
+	case 0:
+		return ""
+	case 1:
+		return strings.Repeat(" ", indent) + items[0]
+	default:
+		// Prepare for multi line string building
+		var (
+			size    int
+			builder strings.Builder
+		)
+		for _, item := range items {
+			size += indent + len(itemPrefix) + len(item) + 1
 		}
+		builder.Grow(size)
+		// Build output
+		for index, item := range items {
+			builder.WriteString(strings.Repeat(" ", indent))
+			builder.WriteString(itemPrefix)
+			builder.WriteString(item)
+			if index != len(items)-1 {
+				builder.WriteRune('\n')
+			}
+		}
+		// Return result
+		return builder.String()
 	}
-	// Return result
-	return builder.String()
 }
